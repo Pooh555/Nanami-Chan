8b6a0ade3e3b133347f225c775aa5fb58034133c
Pooh555 <peerasit.lu@gmail.com>
2025-06-22 23:32:28 +0700

    Create a window using LWJGL

--- app/src/main/java/main/Main.java
+++ app/src/main/java/main/Main.java
@@ -1,70 +1,75 @@
 package main;
 
-import java.io.IOException;
-
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
-
-import llm_wrapper.Ollama;
-import stt.VoskSTT;
-import tts.ElevenlabsTTS;
+import render.WindowManager;
 
 public class Main {
-    public static String personality = "Nanami Osaka";
-    public static String modelName = "llama3:latest";
+	public static void main(String[] args) {
+		WindowManager window = new WindowManager("Nanami Chan", 1600, 900);
+		
+		window.run();
+	}
 
-    public static void main(String[] args) throws Exception {
-        /*
-         * Available model
-         * - Ollama: llama3:lastest (default)
-         * 
-         * Available personalities
-         * - Kita Ikuyo (From ぼっちざろっく!)
-         * - Nanami Osaka (From 現実もたまには嘘をつく) (default)
-         */
-        Ollama model = new Ollama(Main.modelName, Main.personality);
-        /*
-         * Available speech-to-text options
-         *  - Vosk (vosk-model-small-en-us-0.15) (default)
-         */
-        VoskSTT stt = new VoskSTT();
-        /*
-         * Available text-to-speech options
-         * - Elevenlabs (default)
-         */
-        ElevenlabsTTS voice = new ElevenlabsTTS();
-        String userPrompt = "";
-        String recievedMessage;
-        
-        try {
-            System.out.println("\nNanami has woken up. You can talk to her now.");
-
-            while (true) {
-                // Get user prompt using sound-to-text (STT)
-                try {
-                    userPrompt = stt.listenAndTranscribe();
-                } catch (LineUnavailableException | IOException | UnsupportedAudioFileException e) {
-                    throw new Exception("Invalid URI syntax for Ollama API: " + e.getMessage(), e);
-                }
-
-                System.out.println("\nUser: " + userPrompt);
-
-                // Prompt to exit LLM
-                if (userPrompt.equalsIgnoreCase("good bye") || userPrompt.equalsIgnoreCase("goodbye")) {
-                    recievedMessage = model.getResponseText("I have got to go now. Goodbye, see you next time.")
-                            + " さようなら!";
-
-                    System.out.println("\nNanami Chan: " + recievedMessage);
-                    voice.speak(recievedMessage);
-                    break;
-                }
-                recievedMessage = model.getResponseText(userPrompt);
-
-                System.out.println("\nNanami Chan: " + recievedMessage);
-                voice.speak(recievedMessage);
-            }
-        } finally {
-            model.saveConversationHistory();
-        }
-    }
 }
+
+// public class Main {
+//     public static String personality = "Nanami Osaka";
+//     public static String modelName = "llama3:latest";
+
+//         public static void main(String[] args) throws Exception {
+//     }
+
+//     // public static void main(String[] args) throws Exception {
+//     //     /*
+//     //      * Available model
+//     //      * - Ollama: llama3:lastest (default)
+//     //      * 
+//     //      * Available personalities
+//     //      * - Kita Ikuyo (From ぼっちざろっく!)
+//     //      * - Nanami Osaka (From 現実もたまには嘘をつく) (default)
+//     //      */
+//     //     Ollama model = new Ollama(Main.modelName, Main.personality);
+//     //     /*
+//     //      * Available speech-to-text options
+//     //      *  - Vosk (vosk-model-small-en-us-0.15) (default)
+//     //      */
+//     //     VoskSTT stt = new VoskSTT();
+//     //     /*
+//     //      * Available text-to-speech options
+//     //      * - Elevenlabs (default)
+//     //      */
+//     //     ElevenlabsTTS voice = new ElevenlabsTTS();
+//     //     String userPrompt = "";
+//     //     String recievedMessage;
+        
+//     //     try {
+//     //         System.out.println("\nNanami has woken up. You can talk to her now.");
+
+//     //         while (true) {
+//     //             // Get user prompt using sound-to-text (STT)
+//     //             try {
+//     //                 userPrompt = stt.listenAndTranscribe();
+//     //             } catch (LineUnavailableException | IOException | UnsupportedAudioFileException e) {
+//     //                 throw new Exception("Invalid URI syntax for Ollama API: " + e.getMessage(), e);
+//     //             }
+
+//     //             System.out.println("\nUser: " + userPrompt);
+
+//     //             // Prompt to exit LLM
+//     //             if (userPrompt.equalsIgnoreCase("good bye") || userPrompt.equalsIgnoreCase("goodbye")) {
+//     //                 recievedMessage = model.getResponseText("I have got to go now. Goodbye, see you next time.")
+//     //                         + " さようなら!";
+
+//     //                 System.out.println("\nNanami Chan: " + recievedMessage);
+//     //                 voice.speak(recievedMessage);
+//     //                 break;
+//     //             }
+//     //             recievedMessage = model.getResponseText(userPrompt);
+
+//     //             System.out.println("\nNanami Chan: " + recievedMessage);
+//     //             voice.speak(recievedMessage);
+//     //         }
+//     //     } finally {
+//     //         model.saveConversationHistory();
+//     //     }
+//     // }
+// }
---
+++ app/src/main/java/render/WindowManager.java
@@ -0,0 +1,123 @@
+package render;
+
+import java.nio.IntBuffer;
+
+import org.lwjgl.Version;
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.GLFW_FALSE;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
+import static org.lwjgl.glfw.GLFW.GLFW_TRUE;
+import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
+import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
+import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
+import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
+import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
+import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
+import static org.lwjgl.glfw.GLFW.glfwGetWindowSize;
+import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
+import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowShouldClose;
+import static org.lwjgl.glfw.GLFW.glfwShowWindow;
+import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
+import static org.lwjgl.glfw.GLFW.glfwTerminate;
+import static org.lwjgl.glfw.GLFW.glfwWindowHint;
+import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import org.lwjgl.system.MemoryStack;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class WindowManager {
+    private String title;
+    private int width;
+    private int height;
+    private long window;
+
+    public WindowManager(String title, int width, int height) {
+        this.title = title;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void run() {
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+
+		init();
+		loop();
+		glfwFreeCallbacks(window);  // Free the window callbacks
+		glfwDestroyWindow(window);  // Destroy the window
+		glfwTerminate();    // Terminate GLFW
+		glfwSetErrorCallback(null).free();  // Free the error callback
+    }
+
+    private void init() {
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW
+        if (!glfwInit())
+            throw new IllegalStateException("Unable to initialize GLFW");
+
+        // Configure the window
+        glfwDefaultWindowHints();   // Get default window hints
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);   // Hide the window from the user
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);  // Make the window resizable
+
+        window = glfwCreateWindow(this.width, this.height, "Hello World!", NULL, NULL);
+
+        // Check for any error
+        if (window == NULL)
+            throw new RuntimeException("Failed to create the GLFW window");
+
+        // Press escape key to close the window
+        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+                glfwSetWindowShouldClose(window, true);
+        });
+
+        try ( MemoryStack stack = stackPush() ) {
+			IntBuffer pWidth = stack.mallocInt(1);
+			IntBuffer pHeight = stack.mallocInt(1); 
+
+			glfwGetWindowSize(window, pWidth, pHeight); // Pass the window's size to glfwCreateWindow
+
+			// Get the resolution of the primary monitor
+			GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+			// Center the window
+			glfwSetWindowPos(
+				window,
+				(vidmode.width() - pWidth.get(0)) / 2,
+				(vidmode.height() - pHeight.get(0)) / 2
+			);
+		}
+
+		glfwMakeContextCurrent(window); // Set context to current
+		glfwShowWindow(window); // Make the window visible
+    }
+
+    private void loop() {
+		GL.createCapabilities();
+		glClearColor(1.0f, 0.0f, 0.0f, 0.0f); // Set background color
+
+		while ( !glfwWindowShouldClose(window) ) {
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+			glfwSwapBuffers(window); // swap the color buffers
+
+			// Poll for window events. The key callback above will only be
+			// invoked during this call.
+			glfwPollEvents();
+		}
+	}
+}
