15bd96587a5051946dc250247a5950910909767f
Pooh555 <peerasit.lu@gmail.com>
2025-06-22 23:44:00 +0700

    Restructure code

--- app/src/main/java/render/WindowManager.java
+++ app/src/main/java/render/WindowManager.java
@@ -54,12 +54,16 @@ public class WindowManager {
     public void run() {
         System.out.println("Hello LWJGL " + Version.getVersion() + "!");
 
-		init();
-		loop();
-		glfwFreeCallbacks(window);  // Free the window callbacks
-		glfwDestroyWindow(window);  // Destroy the window
-		glfwTerminate();    // Terminate GLFW
-		glfwSetErrorCallback(null).free();  // Free the error callback
+        this.init();
+
+        while (!glfwWindowShouldClose(window)) {
+            this.update();
+        }
+
+        glfwFreeCallbacks(window); // Free the window callbacks
+        glfwDestroyWindow(window); // Destroy the window
+        glfwTerminate(); // Terminate GLFW
+        glfwSetErrorCallback(null).free(); // Free the error callback
     }
 
     private void init() {
@@ -70,9 +74,9 @@ public class WindowManager {
             throw new IllegalStateException("Unable to initialize GLFW");
 
         // Configure the window
-        glfwDefaultWindowHints();   // Get default window hints
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);   // Hide the window from the user
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);  // Make the window resizable
+        glfwDefaultWindowHints(); // Get default window hints
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // Hide the window from the user
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // Make the window resizable
 
         window = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
 
@@ -86,36 +90,36 @@ public class WindowManager {
                 glfwSetWindowShouldClose(window, true);
         });
 
-        try ( MemoryStack stack = stackPush() ) {
-			IntBuffer pWidth = stack.mallocInt(1);
-			IntBuffer pHeight = stack.mallocInt(1); 
+        try (MemoryStack stack = stackPush()) {
+            IntBuffer pWidth = stack.mallocInt(1);
+            IntBuffer pHeight = stack.mallocInt(1);
 
-			glfwGetWindowSize(window, pWidth, pHeight); // Pass the window's size to glfwCreateWindow
+            glfwGetWindowSize(window, pWidth, pHeight); // Pass the window's size to glfwCreateWindow
 
-			// Get the resolution of the primary monitor
-			GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+            // Get the resolution of the primary monitor
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
 
-			// Center the window
-			glfwSetWindowPos(
-				window,
-				(vidmode.width() - pWidth.get(0)) / 2,
-				(vidmode.height() - pHeight.get(0)) / 2
-			);
-		}
+            // Center the window
+            glfwSetWindowPos(
+                    window,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2);
+        }
 
-		glfwMakeContextCurrent(window); // Set context to current
-		glfwShowWindow(window); // Make the window visible
+        glfwMakeContextCurrent(window); // Set context to current
+        glfwShowWindow(window); // Make the window visible
+        GL.createCapabilities();
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Set background color
+
     }
 
-    private void loop() {
-		GL.createCapabilities();
-		glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Set background color
-
-		while ( !glfwWindowShouldClose(window) ) {
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
-			glfwSwapBuffers(window); // swap the color buffers
-            glClearColor((float)(Math.sin(System.currentTimeMillis() / 1000.0)), (float)(Math.cos(System.currentTimeMillis() / 1000.0)), (float)(Math.tan(System.currentTimeMillis() / 1000.0)), 0.0f); // Set background color
-			glfwPollEvents();
-		}
-	}
+    private void update() {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+        glfwSwapBuffers(window); // swap the color buffers
+        // glClearColor((float)(Math.sin(System.currentTimeMillis() / 1000.0)),
+        // (float)(Math.cos(System.currentTimeMillis() / 1000.0)),
+        // (float)(Math.tan(System.currentTimeMillis() / 1000.0)), 0.0f); // Set
+        // background color
+        glfwPollEvents();
+    }
 }
