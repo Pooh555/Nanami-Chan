344266200549fe0d4206d2ec32d78be82cc7c6ae
Pooh555 <peerasit.lu@gmail.com>
2025-06-22 14:09:19 +0700

    Move API_keys.java

--- app/src/main/keys/API_keys.java
+++
@@ -1,6 +0,0 @@
-package keys;
-// Add the API keys that you wanted to use here.
-
-public interface  API_keys {
-    public static final String ELEVENLABS_API_KEY = "sk_a131f1262e30f78d37644361eaa6b4e25effd49c7ed9d0aa";
-}
