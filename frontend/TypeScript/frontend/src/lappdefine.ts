d88738f91bc48e78d9f31d26ca3c4f095a7bb927
Pooh555 <peerasit.lu@gmail.com>
2025-06-23 22:25:25 +0700

    Remove Live2D models: Haru, Mark, Natori, Rice, Wanko

--- frontend/TypeScript/frontend/src/lappdefine.ts
+++ frontend/TypeScript/frontend/src/lappdefine.ts
@@ -48,13 +48,8 @@ export const PowerImageName = 'CloseNormal.png';
 // モデルを配置したディレクトリ名の配列
 // ディレクトリ名とmodel3.jsonの名前を一致させておくこと
 export const ModelDir: string[] = [
-  'Haru',
   'Hiyori',
-  'Mark',
-  'Natori',
-  'Rice',
   'Mao',
-  'Wanko'
 ];
 export const ModelDirSize: number = ModelDir.length;
 
